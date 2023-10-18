14f78976869aa3e3e8d827137a12211e0bfa8780
Soumya <soumyaguptabond@gamil.com>
2023-10-18 17:00:56 +0530

    renamed

--- ios/rn_ecommTests/rn_ecommTests.m
+++ ios/rn_ecommTests/rn_ecommTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rn_ecommTests : XCTestCase
+@interface adminpanelTests : XCTestCase
 
 @end
 
-@implementation rn_ecommTests
+@implementation adminpanelTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
